3a01c9acd006406dd38233df379bf14c6ff76100
xicocaio <xicocaio@gmail.com>
2013-07-17 22:23:43 +0000

    Refactor

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
 var fs = require('fs');
+var htmlfile = "index.html";
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  response.send(fs.readFileSync('index.html').toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
